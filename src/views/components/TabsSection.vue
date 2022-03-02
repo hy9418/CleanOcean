aa241bad3fedde64b47b6410543996288df5c0f2
yuhu.yu <yuhu.yu@alibaba-inc.com>
2022-03-02 17:56:44 +0800

    add project

---
+++ src/views/components/TabsSection.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="wrapper">
+    <div id="nav-tabs">
+      <h3>活动广场</h3>
+      <div class="md-layout">
+        <div class="md-layout-item md-size-50 md-small-size-100">
+          <h3><small>活动标题1</small></h3>
+          <nav-tabs-card no-label>
+            <template slot="content">
+              <md-tabs class="md-primary" md-alignment="left">
+                <md-tab id="tab-home" md-label="活动主题" md-icon="face">
+                  <p>
+                    为了保护海岸线生态，招募志愿者参与指定海岸线的卫生维护
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-pages" md-label="详情介绍" md-icon="chat">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. I will be the leader of a company that ends up
+                    being worth billions of dollars, because I got the answers.
+                    I understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-posts" md-label="参与方式" md-icon="build">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. So when you get something that has the name
+                    Kanye West on it, it’s supposed to be pushing the furthest
+                    possibilities. I will be the leader of a company that ends
+                    up being worth billions of dollars, because I got the
+                    answers. I understand culture. I am the nucleus.
+                  </p>
+                </md-tab>
+              </md-tabs>
+            </template>
+          </nav-tabs-card>
+        </div>
+        <div class="md-layout-item md-size-50 md-small-size-100">
+          <h3><small>活动标题2</small></h3>
+          <nav-tabs-card no-label>
+            <template slot="content">
+              <md-tabs class="md-primary" md-alignment="left">
+                <md-tab id="tab-home" md-label="活动主题" md-icon="face">
+                  <p>
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers. I understand
+                    culture. I am the nucleus. I think that’s a responsibility
+                    that I have, to push possibilities, to show people, this is
+                    the level that things could be at. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-pages" md-label="详情介绍" md-icon="chat">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. I will be the leader of a company that ends up
+                    being worth billions of dollars, because I got the answers.
+                    I understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-posts" md-label="参与方式" md-icon="build">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. So when you get something that has the name
+                    Kanye West on it, it’s supposed to be pushing the furthest
+                    possibilities. I will be the leader of a company that ends
+                    up being worth billions of dollars, because I got the
+                    answers. I understand culture. I am the nucleus.
+                  </p>
+                </md-tab>
+              </md-tabs>
+            </template>
+          </nav-tabs-card>
+        </div>
+        <!-- <div class="md-layout-item md-size-50 md-small-size-100">
+          <h3><small>Tabs on Plain Card</small></h3>
+          <nav-tabs-card no-label tabs-plain>
+            <template slot="content">
+              <md-tabs class="md-danger" md-alignment="left">
+                <md-tab id="tab-home" md-label="Home">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. So when you get something that has the name
+                    Kanye West on it, it’s supposed to be pushing the furthest
+                    possibilities. I will be the leader of a company that ends
+                    up being worth billions of dollars, because I got the
+                    answers. I understand culture. I am the nucleus.
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-pages" md-label="Updates">
+                  <p>
+                    I will be the leader of a company that ends up being worth
+                    billions of dollars, because I got the answers. I understand
+                    culture. I am the nucleus. I think that’s a responsibility
+                    that I have, to push possibilities, to show people, this is
+                    the level that things could be at. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                  </p>
+                </md-tab>
+
+                <md-tab id="tab-posts" md-label="History">
+                  <p>
+                    I think that’s a responsibility that I have, to push
+                    possibilities, to show people, this is the level that things
+                    could be at. I will be the leader of a company that ends up
+                    being worth billions of dollars, because I got the answers.
+                    I understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                  </p>
+                </md-tab>
+              </md-tabs>
+            </template>
+          </nav-tabs-card>
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavTabsCard } from "@/components";
+
+export default {
+  components: {
+    NavTabsCard
+  }
+};
+</script>
+
+<style lang="css"></style>
